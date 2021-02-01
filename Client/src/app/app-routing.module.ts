1756ca7d18239c831ccf57289be9b7336228fb9c
Ron Gabbay <>
2021-02-01 16:48:31 +0200

    login page

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 import { AdminMainComponent } from './components/admin-main/admin-main.component';
+import { LoginComponent } from './components/login/login.component';
 import { MailGunComponent } from './components/mail-gun/mail-gun.component';
 import { MainComponent } from './components/main/main.component';
 import { RestaurantsPageComponent } from './components/restaurants-page/restaurants-page.component';
@@ -10,6 +11,7 @@ import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
   { path: '', component: MainComponent, pathMatch: 'full' },
   { path: 'admin', component: AdminMainComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
   { path: 'restaurants', component: RestaurantsPageComponent },
   { path: 'admin/login', component: AdminLoginComponent },
   { path: 'mail', component: MailGunComponent },
@@ -19,7 +21,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabled'
-})],
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Client/src/app/components/login/login.component.ts
+++ Client/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  loginForm: FormGroup
+  loginInvalid = false;
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+
+    })
+  }
+
+  handleSubmit() {
+    console.log(this.loginForm.value)
   }
 
 }
