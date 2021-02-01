9f891fd382148247fd5198d9efc40248075b5006
Ron Gabbay <>
2021-02-01 17:02:54 +0200

    client side login / register page

--- Client/src/app/components/login/login.component.ts
+++ Client/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +10,21 @@ export class LoginComponent implements OnInit {
 
   constructor(private fb: FormBuilder) { }
 
-  loginForm: FormGroup
+  loginForm: FormGroup;
+  registerForm: FormGroup;
   loginInvalid = false;
+  registered = true;
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+    this.registerForm = this.fb.group({
+      fname: ['', Validators.required],
+      lname: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
     })
   }
 
