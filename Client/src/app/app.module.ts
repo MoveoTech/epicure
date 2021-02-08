28429807d9892849ce5231b84d26f9e3978eba7e
Ron Gabbay <>
2021-02-08 15:48:53 +0200

    push check

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -79,5 +79,5 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
   ],
   providers: [{ provide: SWIPER_CONFIG, useValue: DEFAULT_SWIPER_CONFIG }],
   bootstrap: [AppComponent]
-})
+}) 
 export class AppModule { }
